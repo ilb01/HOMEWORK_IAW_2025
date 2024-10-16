5d2c090cd460b38fa49ad1b5c1ac1aa29d842957
ilb01 <ilb01@iesemilidarder.com>
2024-10-16 20:35:50 +0200

    portafolio

---
+++ 2º_IAW_2025/portafolio_ismael_2025/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from './LandingPage'; // Importar el nuevo componente
+import Portfolio from './portfolio'; // Importar el nuevo componente
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
