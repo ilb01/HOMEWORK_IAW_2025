ec7c26f863753da0dc3303105c9d2112a7b88c61
ilb01 <ilb01@iesemilidarder.com>
2024-11-20 16:05:34 +0100

    ejercicios

--- 2º_IAW_2025/ACT-2.1/04_Budget/src/App.jsx
+++ 2º_IAW_2025/ACT-2.1/04_Budget/src/App.jsx
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
-import ExpenseForm from "./components/ExpenseForm";
-import ExpenseList from "./components/ExpenseList";
-import Alert from "./components/Alert";
-import uuid from "uuid/v4";
+import ExpenseForm from "./components/ExpenseForm.jsx";
+import ExpenseList from "./components/ExpenseList.jsx";
+import Alert from "./components/Alert.jsx";
+import {nanoid} from "nanoid";
 
 const initialExpenses = localStorage.getItem("expenses")
   ? JSON.parse(localStorage.getItem("expenses"))
   : [];
 
-function App() {
+export default function App() {
   // all expenses, add expense
   const [expenses, setExpenses] = useState(initialExpenses);
   // single expense
   const [charge, setCharge] = useState("");
   const [amount, setAmount] = useState("");
+
   const [alert, setAlert] = useState({ show: false });
   const [edit, setEdit] = useState(false);
-  const [id, setId] = useState(0);
+
+  const idRef = useRef(0);
 
   useEffect(() => {
     localStorage.setItem("expenses", JSON.stringify(expenses));
@@ -25,70 +27,71 @@ function App() {
 
   // *********** functionality **************
   // Add charge
-  const handleCharge = e => {
+  function handleCharge(e){
     setCharge(e.target.value);
   };
 
   // Add amount
-  const handleAmount = e => {
+  function handleAmount(e){
     let amount = e.target.value;
-    amount === "" ? setAmount(amount) : setAmount(parseInt(amount));
+    (amount)?setAmount(parseInt(amount)):setAmount(amount);
   };
 
   // handle alert
-  const handleAlert = ({ type, text }) => {
+  function handleAlert({ type, text }){
     setAlert({ show: true, type, text });
     setTimeout(() => {
       setAlert({ show: false });
-    }, 7000);
+    }, 3000);
   };
 
   // handle submit
-  const handleSubmit = e => {
+  function handleSubmit(e){
     e.preventDefault();
     // Check data 
     if (charge !== "" && amount > 0) {
       if (edit) {
         // Edit Expense
-        let tempExpenses = expenses.map(item => {
-          return item.id === id ? { ...item, charge, amount } : item;
-        });
-        setExpenses(tempExpenses); // New list
+        setExpenses(expenses.map(item => {
+          return item.id === idRef.current ? { ...item, charge, amount } : item;
+        })); 
+        // New list
         setEdit(false);
       } else {
         // New expense
-        const singleExpense = { id: uuid(), charge, amount };
+        const newExpense = { id:nanoid(), charge:charge, amount:amount };
         // A new array is required
-        setExpenses([...expenses, singleExpense]);
-        handleAlert({ type: "success", text: "item added" });
+        setExpenses([...expenses, newExpense]);
+        handleAlert({ type: "success", text: "Expense added" });
       }
       setCharge("");
       setAmount("");
     } else {
       handleAlert({
         type: "danger",
-        text: `charge can't be empty value and amount value has to be bigger than zero`
+        text: `Charge can't be empty value and amount value has to be bigger than zero`
       });
     }
   };
+
   // handle delete
-  const handleDelete = id => {
-    let tempExpenses = expenses.filter(item => item.id !== id);
-    setExpenses(tempExpenses);
-    handleAlert({ type: "danger", text: "item deleted" });
+  function handleDelete(id){
+    setExpenses(expenses.filter(item => item.id !== id));
+    handleAlert({ type: "danger", text: "Expense deleted" });
   };
+
   // clear all items
-  const handleClearItems = () => {
+  function handleClearItems(){
     setExpenses([]);
   };
+
   // handle edit
-  const handleEdit = id => {
-    let expense = expenses.find(item => item.id === id);
-    let { charge, amount } = expense;
+  function handleEdit(id){
+    let { charge, amount } = expenses.find(item => item.id === id);
     setCharge(charge);
     setAmount(amount);
     setEdit(true);
-    setId(id);
+    idRef.current = id;
   };
 
   return (
@@ -97,28 +100,26 @@ function App() {
       <h1>budget calculator</h1>
       <main className="App">
         <ExpenseForm
-          handleSubmit={handleSubmit}
           charge={charge}
-          handleCharge={handleCharge}
           amount={amount}
+          handleCharge={handleCharge}
           handleAmount={handleAmount}
+          handleSubmit={handleSubmit}
           edit={edit}
         />
         <ExpenseList
           expenses={expenses}
+          clearItems={handleClearItems}
           handleDelete={handleDelete}
           handleEdit={handleEdit}
-          clearItems={handleClearItems}
         />
       </main>
       <h1>
         total spending :
-        <span className="total">    
+        <span className="total">
           {expenses.reduce((acc, curr) => (acc += curr.amount), 0)} €
         </span>
       </h1>
     </>
   );
 }
-
-export default App;
